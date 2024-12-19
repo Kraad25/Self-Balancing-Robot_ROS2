bab81e9bdabc78851911b07de39b423e236cff48
Krishnaprasaad R <rkprasaad12@gmail.com>
2024-12-19 20:48:37 +0900

    Gazebo Visualization

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory("sbr_pjt")
+    urdf_file = os.path.join(pkg_path, 'urdf', 'sbr.urdf')
+
+    robot_description = Command(['xacro ', urdf_file])
+    doc = xacro.parse(open(urdf_file))
+    xacro.process_doc(doc)
+    params={'robot_description':doc.toxml()}
+
+
+    robot_state_publisher = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    output='screen',
+    parameters=[params]
+    )
+
+    # Launch Gazebo and spawn the robot
+    
+    # Launch Gazebo
+    gazebo = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        [os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py'])
+    )
+
+    # Spawn Entity
+    # Spawn the robot in Gazebo
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', '/robot_description', '-entity', 'robot'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(name='headless', default_value='true', description='Set to "false" to run headless.'),
+        gazebo,
+        robot_state_publisher,
+        spawn_entity,
+    ])
